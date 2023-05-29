fcfe3232e417d269e5611e794e6f635abef8ac70
KY-2187 <kyrahung1@gmail.com>
2023-05-29 15:58:54 +0800

    Create operate function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(first, operator, second) {
+    if (operator == '+') {
+        return add(first, second);
+    } else if (operator == '-') {
+        return subtract(first, second);
+    } else if (operator == '*') {
+        return multiply(first, second);
+    } else if (operator == '/') {
+        return divide(first, second);
+    } else {
+        return 'ERROR';
+    }
+}
+
+let number1 = 3;
+let number2 = 2;
+let operator = '/';
+
+console.log(operate(number1, operator, number2));
