9068b65756be382cddebc994f1ae76d66a2a6f4b
KY-2187 <kyrahung1@gmail.com>
2023-05-30 19:21:27 +0800

    Display selected numbers on the screen

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operator');
+const deleteButtons = document.querySelectorAll('.bottom');
+const currentInput = document.querySelector('#current');
+const history = document.querySelector('#history');
+
 function add(a, b) {
     return a + b;
 }
@@ -28,8 +34,44 @@ function operate(first, operator, second) {
     }
 }
 
-let number1 = 3;
-let number2 = 2;
-let operator = '/';
+let input = '0';
+function displayInput() {
+    currentInput.textContent = input;
+}
 
-console.log(operate(number1, operator, number2));
+let number1 = 0;
+let number2 = 0;
+let operator = '/';
+let hasDecimal = false;
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (input.length < 12) {
+            if (input == 0) {
+                input = button.textContent;
+            } else {
+                input += button.textContent;
+            }
+            displayInput();
+            displayCounter++;
+        }
+    })
+})
+
+deleteButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.textContent === 'Delete') {
+            if (input.length == 1) {
+                input = '0';
+            } else {
+                input = input.slice(0, -1);
+            }
+            displayInput();
+        } else if (button.textContent === 'Clear') {
+            input = '0';
+            displayInput();
+        }
+    })
+})
+
+// console.log(operate(number1, operator, number2));
